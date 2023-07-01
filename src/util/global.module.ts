7f1244cacacd89840eef948ab944923a8982eb1c
Hayden Bradfield <hbradfield20@gmail.com>
2023-07-01 18:41:07 -0400

    add HelpersService to GlobalModule

--- src/util/global.module.ts
+++ src/util/global.module.ts
@@ -1,9 +1,10 @@
 import { Global, Module } from '@nestjs/common';
 import { EmailService } from './email.service';
+import { HelperService } from './helpers.service';
 
 @Global()
 @Module({
-    providers: [EmailService], 
-    exports: [EmailService]
+    providers: [EmailService, HelperService], 
+    exports: [EmailService, HelperService]
 })
 export class GlobalModule {}
