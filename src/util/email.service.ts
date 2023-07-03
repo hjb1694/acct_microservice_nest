5ddd48dacecf8eb109ef95297e9898e427ea5021
Hayden Bradfield <hbradfield20@gmail.com>
2023-07-03 12:42:13 -0400

    wip

--- src/util/email.service.ts
+++ src/util/email.service.ts
@@ -9,20 +9,21 @@ export class EmailService {
         private configService: ConfigService
     ){}
 
-    setupTransporter(){
-        return nodeMailer.createTransport({
-            host: this.configService.get('EMAIL_HOST'), 
-            secure: this.configService.get('EMAIL_SECURE') === 'true', 
-            port: +this.configService.get('EMAIL_PORT'), 
-            auth: {
-                user: this.configService.get('EMAIL_USER'), 
-                pass: this.configService.get('EMAIL_PASSWORD')
-            }
-        });
+    connect(){
+
+            return nodeMailer.createTransport({
+                host: this.configService.get('EMAIL_HOST'), 
+                secure: this.configService.get('EMAIL_SECURE') === 'true', 
+                port: +this.configService.get('EMAIL_PORT'), 
+                auth: {
+                    user: this.configService.get('EMAIL_USER'), 
+                    pass: this.configService.get('EMAIL_PASSWORD')
+                }
+            });
     }
 
     async send(to: string, subject: string, content: string){
-        const transporter: nodeMailer.Transporter = this.setupTransporter();
+        const transporter: nodeMailer.Transporter = this.connect();
 
         await transporter.sendMail({
             from: '"Ktown Portal Noreply" <no-reply@ktown-portal.com>', 
