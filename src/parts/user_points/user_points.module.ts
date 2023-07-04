d961c9151cdf5c398cd0a2126403d0809b4b4a9f
Hayden Bradfield <hbradfield20@gmail.com>
2023-07-04 10:34:59 -0400

    import TypeOrmModule in UserPointsModule

--- src/parts/user_points/user_points.module.ts
+++ src/parts/user_points/user_points.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UserPointsController } from './user_points.controller';
 import { UserPointsService } from './user_points.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserPoints } from 'src/db/entities/UserPoints.entity';
 
 @Module({
+    imports: [
+        TypeOrmModule.forFeature([UserPoints])
+    ],
     controllers: [UserPointsController], 
     providers: [UserPointsService]
 })
